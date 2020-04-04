4271453ae483f597996a5ecfa0db87984aebbb11
omarwork <omar_work74@yahoo.com>
2020-04-04 13:26:53 +0200

    version2

--- funccccc.c
+++ funccccc.c
@@ -2,11 +2,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+char * convtoheap(char *arr);
 
 char *read_line(){
+
     char ch;
     int i =0,n=10;
 char *test=(char*) malloc(n);
+puts("shell>>");
 ch=getchar();
 while (ch!='\n' ){
     test[i]=ch;
@@ -20,22 +23,69 @@ while (ch!='\n' ){
 test[i]='\0';
 return test;
 }
-void parse(char* preparse){
+char** parse(char* preparse){
     char preparse_str[strlen(preparse)];
     char* parsed;
-    char ** command=(char**)malloc(8*sizeof(char*));
+    char ** command;
+    char *finalpar;
+    int n=1;
+    //printf("\n%d",strlen(command));
+    printf("\n%d",sizeof(command));
+    printf("\n%d",sizeof(*command));
+    printf("\n%d",sizeof(parsed));
+    printf("\n%d",sizeof(*parsed));
+    printf("\n%d",sizeof(char*));
+    printf("\n%d",sizeof(char**));
+
+    command=(char**)malloc(n*sizeof(char*));
+
+    printf("\n%d",sizeof(command));
     int i=0;
     strcpy(preparse_str,preparse);
-
+puts(preparse_str);
 
     parsed=strtok(preparse_str," ");
     puts(preparse_str);
-    puts(parsed);
-    while(parsed!=NULL){
+  //  puts(parsed);
+    // printf("\n%d parsed>>\n",(int)strlen(&parsed));
+    //printf("\n%d\n",(int)strlen(command));
 
-        command[i]=parsed;
+    printf("\n%d",sizeof(command));
+    while(parsed!=NULL){
+if (i==n){
+        n=n+1;
+        command=realloc(command,n*sizeof(char*));
+        }
+        command[i]=convtoheap(parsed);
         puts(command[i]);
+  //      printf("%d",(int)strlen(command));
+//        printf("\n%d parsed>>\n",(int)strlen(&parsed));
         i++;
         parsed=strtok(NULL," ");
+
     }
+    printf("%d",i);
+    puts("tmam");
+    printf("a");
+
+
+    printf("\n%d",sizeof(command));
+    /*if(command=realloc(command,(i+1))){
+puts("\n allah ynwr");
+    }
+    puts("tmam again");
+    command[i]=NULL;
+    printf("\nthere%s",command[0]);*/
+
+
+    return command;
 }
+char * convtoheap(char *arr){
+char*hamada=(char*)malloc(strlen(arr));
+strcpy(hamada,arr);
+return hamada;
+}
+
+
+
+
--- main.c
+++ main.c
@@ -2,17 +2,67 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 char *read_line();
-void parse(char* preparse);
-
+char** parse(char* preparse);
+char* fun_func();
+int PtopLen(char **ptop);
+void convptoptoarrp(char** ptop,int ln);
 int main()
 {
-    char * ahma;
+
+   char * ahma;
+    char **code_parsed;
+    int status;
+    pid_t child_pid;
+    pid_t waitvar;
     printf("Hello world!\n");
-    //ahma=read_line();
+while(1){
+
+    ahma=read_line();
     //puts(ahma);
-    //printf("\n%d",strlen(ahma));
-    //parse(ahma);
+    puts("here");
+    //printf("\n%d\n",(int)strlen(ahma));
+    code_parsed=parse(ahma);
+    puts(code_parsed[0]);
+    if (strcmp(code_parsed[0],"exit")==0){
+    puts("ok");
+
+    break;
+    }
+    else{
+    puts("not ok");
+    }
+
+    //printf("\nthere%s",code_parsed[0]);
+    child_pid=fork();
+    if (child_pid==0){
+    printf("\nchild:\ncurrent id:%d and child pid%d\n",getpid(),child_pid);
+    puts(code_parsed[0]);
+    //printf("\nthere%s",code_parsed[0]);
+
+    //printf("%d",funny);
+    //convptoptoarrp(code_parsed,funny);
+    //char *argv[] = {"ls", "-a", "-h", NULL};
+
+    if (execvp(code_parsed[0], code_parsed) < 0) {
+                perror(code_parsed[0]);
+                exit(1);
+            }
+    //
+
+         // if (execvp(*code_parsed, code_parsed) < 0) {     /* execute the command  */
+           //    printf("*** ERROR: exec failed\n");
+             //  exit(1);
+         // }
+    }
+    else{
+    waitvar=waitpid(child_pid,&status,WUNTRACED);
+    printf("\nparent\ncurrent id:%d and child pid%d\n",getpid(),child_pid);
+    //puts(code_parsed[0]);
+
+    }
+}
 
     return 0;
 }
